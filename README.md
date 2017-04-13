9f0b9303624c19ac97fb079aef0a5375d7ae9706
huyueb <1062887235@qq.com>
2017-04-13 15:16:15 +0800

    配置初始化

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # moy-rollup
 使用rollup对moy框架进行打包，和webpack打包进行对比
+
+## 体积比较
+不使用压缩的前提：rollup打包出来的比webpack的少大概120kb
+使用压缩的前提：rollup打包出来的比webpack的少大概30kb
---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+    entry: 'src/moy.js',
+    format: 'cjs',
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**' // only transpile our source code
+        })
+        // uglify()
+    ],
+    dest: 'build/bundle.js'
+};
