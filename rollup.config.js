b9046cd20952f8934a4522525c8013a170308b81
huyueb <1062887235@qq.com>
2017-04-13 16:27:11 +0800

    配置初始化

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,39 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
+var type = process.env.TYPE,
+    config = {};
+if (type && type === "build") {
+    config = {
+        dest: "build/moy.min.js",
+        plugins: [
+            resolve({
+                jail: "/"
+            }),
+            uglify(),
+            babel({
+                exclude: 'node_modules/**' // only transpile our source code
+            }),
+        ]
+    }
+} else {
+    config = {
+        dest: "build/moy.js",
+        plugins: [
+            resolve({
+                jail: "/",
+                jsnext: true,
+            }),
+            babel({
+                exclude: 'node_modules/**' // only transpile our source code
+            }),
+        ]
+    }
+}
+
 export default {
     entry: 'src/moy.js',
     format: 'cjs',
-    plugins: [
-        resolve(),
-        babel({
-            exclude: 'node_modules/**' // only transpile our source code
-        })
-        // uglify()
-    ],
-    dest: 'build/bundle.js'
+    plugins: config.plugins,
+    dest: config.dest
 };
