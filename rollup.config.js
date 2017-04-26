ff3b764c0e7a3a83c6cbf2b9b8ec7e6709ad702a
huyueb <1062887235@qq.com>
2017-04-26 19:24:05 +0800

    增加相应的注释

--- rollup.config.js
+++ rollup.config.js
@@ -12,28 +12,28 @@ export default {
     moduleName: "bar",
     plugins: [
         license({
+            //加入文件注释头
             banner: `<%= pkg.name %> v<%= pkg.version %>
                     <%= pkg.description %>
                     author : <%= pkg.author %>
                     homepage : <%= pkg.homepage %>
                     bugs : <%= pkg.bugs.url %>`,
         }),
-        nodeResolve(),
+        nodeResolve(),//将第三方库打包进入口文件
         multidest([
-            // targets "main" in package.json
+            // 提供可以一次性编译出多个版本
             {
                 dest: "dist/moy.cjs.js",
-                format: "cjs"
+                format: "cjs"//按照cmd格式编译打包
             },
-            // targets browsers
             {
                 dest: "dist/moy.min.js",
-                format: "iife",
-                plugins: [uglify({}, minify)]
+                format: "iife",//按照<script>标签引入到浏览器中能识别的格式
+                plugins: [uglify({}, minify)]//压缩
             }
         ]),
         babel({
-            exclude: "node_modules/**" // only transpile our source code
+            exclude: "node_modules/**" // 排除node_modules文件夹
         })
     ]
 };
